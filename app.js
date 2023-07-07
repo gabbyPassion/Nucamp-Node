debe4e625d9257c7bf98d1de803fbb10fd448740
gabbyPassion <129009206+gabbyPassion@users.noreply.github.com>
2023-07-07 16:38:30 -0400

    node module examples

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-const rect = {
-    perimeter: (x, y) => 2 * (x + y),
-    area: (x, y) => x * y
-};
+const rect = require('./rectangle');
 
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
@@ -18,3 +15,4 @@ solveRect(2, 4);
 solveRect(3, 5);
 solveRect(0, 5);
 solveRect(5, -3);
+
